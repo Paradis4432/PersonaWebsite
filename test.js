df2854761a4bb6c396000bb4e8a8bd76cc03de30
Paradis4432 <Lucasezequiel120202@gmail.com>
2021-12-20 16:21:24 -0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,20 @@
+//create a json with 10 random cars
+const cars = [
+    {
+        "id": 1,
+        "make": "Ford",
+        "model": "Fiesta",
+        "year": "2018",
+        "price": "£12,000",
+
+    },
+    {
+        "id": 2,
+        "make": "Ford",
+        "model": "Focus",
+        "year": "2018",
+        "price": "£12,000",
+
+    },
+]
+console.table(cars)
